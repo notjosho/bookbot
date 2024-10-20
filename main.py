d615002ba03d463619ce22ee5317018ba4087969
notjosho <joshopascal@gmail.com>
2024-10-20 08:58:08 -0600

    Added bookbot python project for counting words and logging

---
+++ main.py
@@ -0,0 +1,33 @@
+import re
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    get_chars(text)
+
+def get_chars(text):
+    chars_dictionary = {}
+    for char in text:
+        lower_case_char = char.lower()
+        if lower_case_char not in chars_dictionary:
+            chars_dictionary[lower_case_char] = 1
+        else: 
+            chars_dictionary[lower_case_char] += 1
+    log_chars(chars_dictionary, get_num_words(text))
+
+def log_chars(chars_dictionary, total_chars): 
+    print('--- Begin report books/frankenstein.txt ---')
+    print(f'{total_chars} words found in the document \n')
+    for key, value in chars_dictionary.items():
+        print(f"The '{key}' character was found {value} times")
+    print('--- End report ---')
+
+def get_num_words(text):
+    return len(text.split())
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+main()
